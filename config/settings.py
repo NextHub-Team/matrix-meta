8e71e38b4cc369040a07cfad1c4e155f1c3d4ca9
Rozhan-rhi <rojanrahmani2002@yahoo.com>
2025-01-23 12:40:24 +0330

    fix: add server ip and domian to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = "django-insecure-b3x39&*iqh1+1=#(6lsd^_&3a%9941cri4c*!26=nagda413q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0","localhost"]
-
+ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0","localhost","185.73.113.2","next-hub.dev"]
 
 # Application definition
 
